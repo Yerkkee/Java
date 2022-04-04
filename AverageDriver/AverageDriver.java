5975685ec399f38e706fa924a876a947300d8a6d
Yerkebulan Zhaiylkan <yerkkee@gmail.com>
2022-04-05 00:35:18 +0200

    Create AverageDriver.java

---
+++ AverageDriver/AverageDriver.java
@@ -0,0 +1,13 @@
+/**
+Name: Yerkebulan Zhaiylkan
+Date: 24.11.2021
+**/
+
+public class AverageDriver {
+
+    public static void main(String[] args) {
+        Average aver = new Average();
+        System.out.println(aver.toString());
+
+    }
+}
