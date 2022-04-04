7e2d4e974c2920fd3ce5630f8a016027990baa0a
Yerkebulan Zhaiylkan <yerkkee@gmail.com>
2022-04-05 00:26:12 +0200

    Create Main.java
    This main Geometry.java class

---
+++ Geometry/Main.java
@@ -0,0 +1,12 @@
+/**
+   Name: Yerkebulan Zhaiylkan
+   University: Vistula University
+*/
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Geometry gd = new Geometry();
+
+    }
+}
