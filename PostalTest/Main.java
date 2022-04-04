f38bd6e178e36f71c326e2e4071953231e0742b7
Yerkebulan Zhaiylkan <yerkkee@gmail.com>
2022-04-05 00:53:01 +0200

    Add files via upload

---
+++ PostalTest/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int size = 4;
+        for(int i=0; i<=size;i++){
+            size=size-1;
+        }
+        System.out.println(size);
+    }
+}
